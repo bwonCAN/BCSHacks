f0047468d1b015caf892cf5cf1be451291be6069
Sophia Pobre <sophiapobre@gmail.com>
2023-03-25 12:03:18 -0700

    Added Algorithms topic

---
+++ src/src/Sophia/Algorithms.java
@@ -0,0 +1,15 @@
+package Sophia;
+
+import main.TopicTest;
+
+public class Algorithms extends TopicTest {
+    public Algorithms() {
+        super();
+
+        courseList.add(cpsc121);
+//        courseList.add(cpsc221);
+//        courseList.add(cpsc320);
+//        courseList.add(cpsc420);
+    }
+
+}
